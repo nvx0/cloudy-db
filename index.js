2dfe9c1b4a613262953d677ec63497e12e20bb4a
Jakub Kun <80093182+falseCloud@users.noreply.github.com>
2021-07-05 14:51:45 +0200

    Reversing changes.

--- index.js
+++ index.js
@@ -1,145 +1,231 @@
-const DB = require("simple-json-db");
-const fs = require("fs")
-const sha256 = require("js-sha256")
+const db_path = './mydb.json'
+const db_path_backup = './mydb_backup.json'
+const fs = require('fs');
+const JSONdb = require('simple-json-db');
+const sha256 = require('js-sha256');
 
-class Database {
-    /**
-     * Database creation
-     * @param {object} options - Object with options
-     * @param {string} options.path - Path to a database file
-     */
-    constructor(options = {}) {
-        this.path = options.path && typeof options.path === 'string' ? options.path : "./database.json"
-        this.database = new DB(this.path);
-    }
-
-    /**
-     * Get a value from the database
-     * @param {string} key - Key
-     * @returns {*} - Data
-     */
-    get(key) {
-        if (!key) throw new TypeError("[GET] No key specifiied!")
-        try {
-            return this.database.get(key)
-        } catch (err) {
-            throw new Error(err)
-        }
-    }
-
-    /**
-     * Deletes the item from the database
-     * @param {string} key - Key
-     */
-    delete(key) {
-        if (!key) throw new TypeError("[DELETE] No key specifiied!")
-        try {
-            this.database.delete(key)
-        } catch (err) {
-            throw new Error(err)
-        }
-    }
-
-    /**
-     * Checks if the item is in the database
-     * @param {string} key - Key
-     * @returns {boolean} Boolean
-     */
-    has(key) {
-        if (!key) throw new TypeError("[HAS] No key specifiied!")
-        return this.database.has(key)
-    }
-
-    /**
-     * Set value
-     * @param {string} key - Key
-     * @param {string} value - Value
-     */
-    set(key, value) {
-        if (!key || !value) throw new TypeError("[SET] No key or value specifiied!")
-        try {
-            return this.database.set(key, value)
-        } catch (err) {
-            throw new Error(err)
-        }
-    }
-
-    /**
-     * Delete
-     */
-    deleteAll() {
-        return this.database.deleteAll();
-    }
-
-    /**
-     * Deletes everything from the database
-     * @param {string} path - Path to backup
-     */
-    backup(path) {
-        if(!path) throw new TypeError("[BACKUP] No path specifiied!")
-        try {
-            fs.readFile(this.path, function (err, data) {
-                if (err) throw err;
-                fs.writeFile(path, data, function (err) {
-                    if (err) throw err;
-                })
-            })
-        } catch (e) {
-            throw new Error(e)
-        }
-    }
-    
-    cleandb(path) {
-        try {
-            fs.writeFile(this.path, "", function(err) {
-                if(err) {
-                    return console.log(err);
-                }})
-                return console.log(`[MYDB] Successfully cleaned DB!`) && console.log(`[MYDB.INFO] If you made mistake check your backup file!`)
-        } catch (err) {
-            throw new TypeError(err)
+    if (fs.existsSync(db_path_backup)) {
+      // pass
+    } else {
+        fs.writeFile("./mydb_backup.json", "", function(err) {
+            if(err) {
+                return console.log(err);
             }
+        });         
+    }
+
+    if (fs.existsSync(db_path)) {
+        // pass
+      } else {
+          fs.writeFile("./mydb.json", "", function(err) {
+              if(err) {
+                  return console.log(err);
+              }
+          });         
+      }
+
+const db = new JSONdb('./mydb.json');
+
+try {
+
+exports.set = function set(name, key) {
+    try {
+    if (!name || !key) {
+        throw new TypeError("[CLOUDY-DB] No name or key specified for set() Expected: string")
+        } else {
+            return db.set(name, key);
         }
-
-    cleanbackup(path) {
-        try {
-            fs.writeFile(this.path, "", function(err) {
-                if(err) {
-                    return console.log(err);
-                }})
-                return console.log(`[MYDB] Successfully cleaned backup DB!`) && console.log(`[MYDB.INFO] If you made mistake check your main DB file!`)
-        } catch (err) {
-            throw new TypeError(err)
-            }
-        }
-
-    /**
-     * Extract data from database
-     * @param {string} fileName - File to extract
-     * @param {boolean} encrypted
-     */
-    extract(fileName, encrypted) {
-        if (!fileName || !encrypted) throw new TypeError("[EXTRACT] No fileNAme or encrypted specifiied!")
-        const writefile = `./${fileName}.json`
-
-        fs.readFile(this.path, function (err, data) {
-            if (err) throw err;
-
-            if (encrypted === true) {
-                const datanew = sha256(data);
-                fs.writeFile(writefile, datanew, function (err) {
-                    if (err) throw err;
-                    console.log(`[EXTRACT] Exported file to ${fileName}.json with hashing!`);
-                });
-
-            } else {
-                fs.writeFile(writefile, data, function (err) {
-                    if (err) throw err;
-                    console.log(`[EXTRACT] Exported file to ${fileName}.json without hashing!`);
-                });
-            }
-        });
+    } catch (err) {
+        throw new TypeError(err)
     }
 }
 
-module.exports = Database;
+exports.get = function get(key) {
+    try {
+    if (!key) {
+        throw new TypeError("[CLOUDY-DB] No key for get() specifiied. Expected: string")
+        } else {
+            return db.get(key);
+        }
+    } catch (err) {
+        throw new TypeError(err)
+    }
+}
+
+exports.del = function del(name) {
+    try {
+    if (!name) {
+        throw new TypeError("[CLOUDY-DB] No name or key specified for del() Expected: string")
+        } else {
+            return db.delete(name);
+        }
+    } catch (err) {
+        throw new TypeError(err)
+    }
+}
+
+exports.has = function has(key) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!key) {
+                throw new TypeError(`[CLOUDY-DB] No name or key specified for has() Expected: string`)
+            } else {
+                return db.has(key);
+            }
+        } catch (err) {
+                throw new TypeError(err)
+        }
+    })
+}
+
+exports.sync = function sync() {
+    try {
+       return db.sync();
+    } catch (err) {
+        throw new TypeError(err)
+    }
+}
+} catch (err) {
+    console.error("[CLOUDY-DB] Cannot setup DB! Operation aborted!")
+    console.error("[CLOUDY-DB] If no error is specified please open a issue on github and give your code!")
+    throw new TypeError(err)
+} 
+
+exports.backup = function backup(type) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!type) {
+                throw new TypeError("[CLOUDY-DB] No name or key specified for type() Expected: string")
+                } else {
+                    if (type === "save") {
+                        try {
+                            fs.readFile(db_path, function (err, data) {
+                                if (err) throw err;
+                                fs.writeFile(db_path_backup, data, function (err) {
+                                    if (err) throw err;
+                                    if (type !== "save") {
+                                        console.error(`[CLOUDY-DB] Coudln't save backup! ${type} is not one of two (save/read)`)
+                                    } else {
+                                        return console.log("[CLOUDY-DB] Succesfully saved backup!") 
+                                    }
+                                });
+                            });
+                        } catch (err) {
+                            console.error("[CLOUDY-DB] If no error is specified please open a issue on github and give your code!")
+                            throw new TypeError(err)
+                        }
+                    }
+    
+                    if (type === "read") {
+                        try {
+                            fs.readFile(db_path_backup, function (err, data) {
+                                if (err) throw err;
+                                fs.writeFile(db_path, data, function (err) {
+                                    if (err) throw err;
+                                    if (type !== "read") {
+                                        console.error(`[CLOUDY-DB] Coudln't save backup! ${type} is not one of two (save/read)`)
+                                    } else {
+                                        return console.log("[CLOUDY-DB] Succesfully read & wrote backup!") 
+                                    }
+                                });
+                            });
+                        } catch (err) {
+                            console.error("[CLOUDY-DB] If no error is specified please open a issue on github and give your code!")
+                            throw new TypeError(err)
+                        }
+                    }
+                }
+            } catch (err) {
+                throw new TypeError(err)
+            }
+    })}
+    
+	exports.extract = function extract(filename, encrypted) {
+        if (!filename) {
+            throw new TypeError("[CLOUDY-DB] Missing filename for extract()")
+        } else {
+        if (!encrypted) {
+            throw new TypeError("[CLOUDY-DB] Missing hashed for extract()")
+        } else {
+            const writefile = `./${filename}.json`
+    
+            fs.readFile(db_path, function (err, data) {
+                if (err) throw err; 
+        
+                if (encrypted === "true") {
+                    const datanew = sha256(data);
+                    fs.writeFile(writefile, datanew, function (err) {
+                        if (err) throw err;
+                        console.log(`[CLOUDY-DB] Exported file to ${filename}.json with hashing!`);
+                        }); 
+                
+                } else {
+                    fs.writeFile(writefile, data, function (err) {
+                        if (err) throw err;
+                        console.log(`[CLOUDY-DB] Exported file to ${filename}.json without hashing!`);
+                        });
+                }});
+            }
+        }
+    }
+    
+    exports.subtract = function subtract(key, value) {
+        if (!key) {
+            throw new TypeError("[CLOUDY-DB] Missing db key for subtract()")
+        } else {
+        if (!value) {
+            throw new TypeError("[CLOUDY-DB] Missing value to subtract for subtract()")
+        } else {
+            try {
+                    const math = `${db.get(key)-value}`
+                    db.set(key, math)
+                    return console.log(`[CLOUDY-DB] Successfully subtracted!`)
+            } catch (err) {
+                throw new TypeError(err)
+                }
+            }
+        }
+    }
+
+
+	exports.add = function add(key, value) {
+    if (!key) {
+        throw new TypeError("[CLOUDY-DB] Missing db key for add()")
+    } else {
+    if (!value) {
+        throw new TypeError("[CLOUDY-DB] Missing value to subtract for add()")
+    } else {
+        try {
+                const math = `${db.get(key)+value}`
+                db.set(key, math)
+                return console.log(`[CLOUDY-DB] Successfully added!`)
+        } catch (err) {
+            throw new TypeError(err)
+            }
+        }
+    }
+}
+
+exports.cleandb = function cleandb() {
+        try {
+            fs.writeFile(db_path, "", function(err) {
+                if(err) {
+                    return console.log(err);
+                }})
+                return console.log(`[CLOUDY-DB] Successfully cleaned DB!`) && console.log(`[CLOUDY-DB.INFO] If you made mistake check your backup file!`)
+        } catch (err) {
+            throw new TypeError(err)
+            }
+        }
+
+exports.cleanbackup = function cleanbackup() {
+        try {
+            fs.writeFile(db_path_backup, "", function(err) {
+                if(err) {
+                    return console.log(err);
+                }})
+                return console.log(`[CLOUDY-DB] Successfully cleaned backup DB!`) && console.log(`[CLOUDY-DB.INFO] If you made mistake check your main DB file!`)
+        } catch (err) {
+            throw new TypeError(err)
+            }
+        }
